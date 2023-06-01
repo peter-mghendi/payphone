bbbb977d9c07b9f4a8e18dc81947a93945276868
sixpeteunder <plenjo15@gmail.com>
2023-06-01 18:05:05 +0300

    Add code to call into shared library via bindings

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
+import java.lang.foreign.MemorySegment;
+import java.lang.foreign.SegmentAllocator;
+import java.lang.foreign.SegmentScope;
+
+import static org.example.libsample.LibSample.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int a = 8;
+        int b = 2;
+
+        // Sum
+        int sum = add(a, b);
+        System.out.printf("The sum is %d.\n", sum);
+
+        // Subtract
+        int difference = subtract(a, b);
+        System.out.printf("The difference is %d.\n", difference);
+
+        // Multiply
+        int product = multiply(a, b);
+        System.out.printf("The product is %d.\n", product);
+        
+        // Greet
+        // Passing strings require a little more legwork. 
+        // We allocate the string to a memory segment off-heap and pass the pointer to that to the native code.  
+        SegmentAllocator allocator = SegmentAllocator.nativeAllocator(SegmentScope.auto());
+        MemorySegment generalKenobi = allocator.allocateUtf8String("General Kenobi");
+        MemorySegment result = greet(generalKenobi);
+        String greeted = result.getUtf8String(0);
+        System.out.printf("%s.\n", greeted);
+
+        // Greet with a mull pointer
+        String nullGreeted = greet(MemorySegment.NULL).getUtf8String(0);
+        System.out.printf("%s.\n", nullGreeted);
     }
 }
